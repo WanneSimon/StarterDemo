5e9a934ba8263ae5eeae2697b6e75c471dd81799
wanne <wanneseville@foxmail.com>
2020-07-10 17:41:04 +0800

    what's this error?

--- src/main/java/cc/wanforme/nukkit/StarterDemo.java
+++ src/main/java/cc/wanforme/nukkit/StarterDemo.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import cc.wanforme.nukkit.spring.NukkitApplicationLauncher;
+
 @SpringBootApplication
 public class StarterDemo extends NukkitApplicationLauncher{
 	private static final Logger log = LoggerFactory.getLogger(StarterDemo.class);
