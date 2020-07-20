8b4cbd0126381bf0c6f82158fe12d12f75d27c0d
wanne <wanneseville@foxmail.com>
2020-07-20 13:01:47 +0800

    add run.bat

---
+++ Run.bat
@@ -0,0 +1,6 @@
+%It is recommended for you to use "java -jar  StarterDemo-1.0-SNAPSHOT.jar", If you run on your development-machine %
+
+@echo "Starting ..."
+@echo "Downloading libraries from maven repository, it may takes a few minutes. You can also place libraries into repository."
+java -Dthin.root=. -jar  StarterDemo-1.0-SNAPSHOT.jar
+PAUSE
