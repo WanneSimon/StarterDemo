9d3e9f4d3b594aff6d055d7e493cbae1c9691f9d
wanne <wanneseville@foxmail.com>
2020-07-21 11:42:38 +0800

    update reamde

--- Run.bat
+++ Run.bat
@@ -1,4 +1,5 @@
 %It is recommended for you to use "java -jar  StarterDemo-1.0-SNAPSHOT.jar", If you run on your development-machine %
+@ecoh This must be place with StarterDemo-1.0-SNAPSHOT.jar
 
 @echo "Starting ..."
 @echo "Downloading libraries from maven repository, it may takes a few minutes. You can also place libraries into repository."
